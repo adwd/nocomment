c6adcdfcff06f3c8f04033bd5b0874774379c129
adwd <ecdt.mn@gmail.com>
2019-06-15 16:50:54 +0900

    fix popup

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,48 +1,11 @@
-import moment from 'moment';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let count = 0;
-
-const time = moment().format('YYYY-MM-DD HH:mm:ss');
-
 const App = () => {
-  const [count, setCount] = React.useState(0);
-  const [url, setUrl] = React.useState('');
-  React.useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      setUrl(tabs[0].url || '');
-    });
-  }, []);
-
-  const handleCountUp = () => {
-    const c = count + 1;
-    setCount(c);
-    chrome.browserAction.setBadgeText({ text: count.toString() });
-  };
-
-  const handleChangeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      chrome.tabs.sendMessage(
-        tabs[0].id,
-        {
-          color: '#555555',
-        },
-        function(msg) {
-          console.log('result message:', msg);
-        },
-      );
-    });
-  };
-
   return (
     <>
-      <ul style={{ minWidth: '700px' }}>
-        <li>Current URL: {url}</li>
-        <li>Current Time: {time}</li>
-      </ul>
-      <button onClick={handleCountUp}>count up</button>
-      <button onClick={handleChangeBackground}>change background</button>
+      <h2>NoComment</h2>
+      <p>removes yahoo comment from news.yahoo.co.jp</p>
     </>
   );
 };
